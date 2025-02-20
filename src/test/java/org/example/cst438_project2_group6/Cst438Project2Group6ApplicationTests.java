892404810e376b34c136d6850670c1aa18b70e9c
Chris Rensel-Smith <32113009+0Starus@users.noreply.github.com>
2025-02-20 15:38:40 -0800

    initial

---
+++ src/test/java/org/example/cst438_project2_group6/Cst438Project2Group6ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.cst438_project2_group6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cst438Project2Group6ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
