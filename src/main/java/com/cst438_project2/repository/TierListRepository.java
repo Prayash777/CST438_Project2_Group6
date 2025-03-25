4e573d80988ba96e682c51e1014cbf859b9bd118
Prayash Raj Singh <prayashsingh701@gmail.com>
2025-03-25 14:00:02 -0700

    New changes to tierlist

--- src/main/java/com/cst438_project2/repository/TierListRepository.java
+++ src/main/java/com/cst438_project2/repository/TierListRepository.java
@@ -1,7 +1,12 @@
 package com.cst438_project2.repository;
 
+import com.cst438_project2.model.User;
+import com.cst438_project2.model.Categories;
 import com.cst438_project2.model.TierEntry;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
 public interface TierListRepository extends JpaRepository<TierEntry, Integer> {
+    List<TierEntry> findByUserAndCategory(User user, Categories category);
+    List<TierEntry> findTierEntriesByUser(User user);
 }
